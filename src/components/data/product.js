ddb6e4c35e2b7c6f2de9d9fcfd32ed3cfbab4c13
islem12342p <liomcxx@gmail.com>
2023-11-17 18:53:50 +0100

    v1

--- src/components/data/product.js
+++ src/components/data/product.js
@@ -1,11 +1,10 @@
 // product.js
 const product = {
-    name: "La Jolíe Muse Christmas Candles, Pine Scented Candle Gifts for Women, Green Candles, Winter Candles, Pomegranate Scented Candles, Luxury Glass Jar Natural Soy Wax Candle, 55 Hours, 8.1Oz/230g ",
+    name: "La Jolíe Muse Christmas Candles,55 Hours, 8.1Oz/230g ",
     price: 19.99,
     description: <ul>
-        <li>SIZE AND BURN TIME - 8.1oz/230g. Featuring natural wax and a lead-free cotton wick, our vegan and cruelty-free candle sends you on a pleasant sensory journey for 45-55 hours. Paraben-free and paraffin-free.</li>
-        <li>CONIFEROUS ZEST - The tartness of freshly picked pomegranate and mandarin oranges meet the freshness of fir needle, fir balsam, and eucalyptus to create a balanced yet alluring bouquet suitable for all seasons.</li>
-        <li>BLISSFUL INDULGENCE - The fragrance diffuses quickly for an almost instant indulgence with a guaranteed clean and even burn.</li>
+        <li>SIZE AND BURN TIME - 8.1oz/230g.Featuring natural wax and a lead-free cotton wick.</li>
+        <li>CONIFEROUS ZEST - The tartness of freshly picked pomegranate and mandarin oranges meet the freshness of fir needle.</li>
     </ul>,
     imagePath: "Images/candles.jpg"
 };
